d0c654b4cc87fcf5706dcb09c0018f1c3fedb112
KamleshPandey6261 <kamlesh.pandey@adixoo.com>
2025-10-04 06:17:13 -0700

    initial commit

---
+++ List.py
@@ -0,0 +1,23 @@
+list=[10,20,30,40,50]
+list2=["Tam",True,"Shyam",10,40.50]
+print(list)
+print(list2)
+print(list[0])
+print(len(list2))
+print(list[1::])
+print(list[::-1])
+#add the element
+list.append(00);
+print(list)
+list.insert(0,43)
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+
+# Remove the element
+#list.remove(10)
+print(list)
+list.pop(0)
+print(list)
