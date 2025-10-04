ae427e88a69dcea05e897d6eda4b8bf7740ab47f
KamleshPandey6261 <kamlesh.pandey@adixoo.com>
2025-10-04 06:17:59 -0700

    initial commit

---
+++ LIstp1.py
@@ -0,0 +1,9 @@
+movies=[]
+m1=input("Enter the first movies name")
+m2=input("Enter the second movies name")
+m3=input ("enter the thired movies name")
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
+movies.sort()
+print(movies)
